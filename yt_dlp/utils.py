904a7999de67f426a67cd654bfaea643b01b5a66
Pascal <pascal.12.22@web.de>
2023-05-22 12:41:26 +0200

    Always return `default_val` when empty

--- yt_dlp/utils.py
+++ yt_dlp/utils.py
@@ -6580,10 +6580,12 @@ class LangSelector:
                 if isinstance(section, str) else section
             return list(map(expr_normalizer, expressions))
 
-        selection = selection or ([[default]] if default else [])
+        default = [[default]] if default else []
+        selection = selection or default
         sections = (re.split(section_sep, selection) if section_sep else [selection]) \
             if isinstance(selection, str) else selection
-        return list(filter(None, map(section_normalizer, filter(None, sections)))), expr_formatter
+        return list(filter(None, map(section_normalizer, filter(None, sections)))) \
+            or default, expr_formatter
 
     @staticmethod
     def format_expr(expr, formatter):
